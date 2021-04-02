e2d0eed4ab19d89b5111cd6d39b5547e0395d58a
Hisiter <leeyueshing19@gmail.com>
2021-04-02 11:33:58 +0700

    add download data

--- DATA/download_data.sh
+++ DATA/download_data.sh
@@ -1,14 +1,15 @@
-cd DATA
 mkdir train_hw_real
 cd train_hw_real
 gdown --id 14xTwzbyLdj9d_ndCrSNq-Jff43PTWgBY
 gdown --id 13oz61B0vVKhftwRLUMgHP5HX9OdZ_gL8
 cd ..
 mkdir valid_hw
+cd valid_hw
 gdown --id 1aDyg7pN5DZoiIGu6TPf9UtMGuAtzphtC
 gdown --id 1gQB3mnfQ0ANPF5kJySs9fzrmCI0WOHzj
 
 cd ..
 mkdir train_hw_synth
+cd train_hw_synth
 gdown --id 1RLIy1zu_heExlRYBjlus2d5-3yZQ5XZY
 gdown --id 1mARMR9uAN8G8F4S5a-mAiYqnT97OxGLs
